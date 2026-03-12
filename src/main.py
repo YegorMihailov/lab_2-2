19f8b452dacf818376a1b7f963526a908cf7a16f
Yegor <egor-epeggar-meks@yandex.ru>
2026-03-12 15:17:48 +0300

    initial

--- src/main.py
+++ src/main.py
@@ -1,16 +1,109 @@
 from typing import runtime_checkable, Protocol
 from dataclasses import dataclass
-import random, time, json
+import random, time, json, datetime
+from src.constants import ALLOWED_STATUSES
+
+# удалить description из payload
 
 
-@dataclass
+class IntegerRange:
+    
+    def __init__(self, min_value: int = 1, max_value: int = None):
+        self.min_value = min_value
+        self.max_value = max_value
+    
+    def __set_name__(self, owner, name):
+        self.private_name = f"_{name}"
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return getattr(instance, self.private_name)
+    
+    def __set__(self, instance, value):
+        if not isinstance(value, int):
+            raise TypeError()
+        if value < self.min_value:
+            raise ValueError(f"Min value is {self.min_value}")
+        if self.max_value and value > self.max_value:
+            raise ValueError(f"Max value is {self.max_value}")
+        
+        setattr(instance, self.private_name, value)
+
+
 class Task:
     """Unit of work with id and data payload"""
 
-    id: int
+    __slots__ = ('_id', '_priority', '_status', '_created_at', 'payload')
+
+    id = IntegerRange(min_value=1)
+    priority = IntegerRange(min_value=1, max_value=5)
     payload: dict
 
+    def __init__(self, id: int, description: str, priority: int, payload = None):
+        self.payload = payload or {}
+        self.id = id
+        self.status = 'created'
+        self.description = description
+        self.priority = priority
 
+        self._created_at = datetime.datetime.now()      
+
+    def __repr__(self):
+        return f"Task(id={self.id}, status='{self.status}', priority={self.priority}, description={self.description}, created_at={self.created_at})"  
+
+    @classmethod
+    def verify_status(cls, status):
+        if not isinstance(status, str):
+            raise TypeError('Status should be string')
+        if status not in ALLOWED_STATUSES:
+            raise ValueError('Unacceptable status')
+
+    @property
+    def status(self) -> str:
+        return self._status
+    
+    @status.setter
+    def status(self, status):
+        self.verify_status(status)
+        self._status = status
+
+    @classmethod
+    def verify_description(cls, description):
+        if not isinstance(description, str):
+            raise TypeError('Description should be string')
+        if len(description) <= 5:
+            raise ValueError('Description must be at least 5 characters long')
+
+    @property
+    def description(self) -> str:
+        return self.payload.get('description', '')
+    
+    @description.setter
+    def description(self, description):
+        self.verify_description(description)
+        self.payload['description'] = description
+    
+    @property
+    def created_at(self) -> datetime.datetime:
+        return self._created_at
+    
+    def __getattr__(self, name):
+        if name in self.payload:
+            return self.payload[name]
+        
+        raise AttributeError('No such attribute')
+    
+    @property
+    def ready_to_start(self) -> bool:
+        try:
+            self.verify_description(self.description)
+        
+            return self._status == 'created'
+        except (TypeError, ValueError):
+            return False
+    
+    
 @runtime_checkable
 class TaskSource(Protocol):
     """Structural protocol defining the interface"""
@@ -26,7 +119,7 @@ class GeneratorTaskSource:
     def get_tasks(self) -> list[Task]:
         """Generate random tasks"""
 
-        tasks = [Task(id=random.randint(1, 100), payload={"order_id": random.randint(1, 1000), "amount": random.randint(100, 1000)}) for i in range(random.randint(1, 5))]
+        tasks = [Task(id=random.randint(1, 100), payload={"order_id": random.randint(1, 1000), "amount": random.randint(100, 1000)}, description='Task description', priority=random.randint(1, 5)) for i in range(random.randint(1, 5))]
         return tasks
 
 
@@ -37,10 +130,10 @@ class ApiTaskSource:
         """Simulate API call to fetch tasks with a delay"""
         time.sleep(1)
         return [
-            Task(id=101, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}),
-            Task(id=102, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}),
-            Task(id=103, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}),
-            Task(id=104, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)})
+            Task(id=101, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}, description='Task description', priority=random.randint(1, 5)),
+            Task(id=102, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}, description='Task description', priority=random.randint(1, 5)),
+            Task(id=103, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}, description='Task description', priority=random.randint(1, 5)),
+            Task(id=104, payload={"order_id": random.randint(1001, 2000), "amount": random.randint(1000, 2000)}, description='Task description', priority=random.randint(1, 5))
         ]
     
 
@@ -57,7 +150,7 @@ class FileTaskSource:
         try:
             with open(self.filename, "r") as f:
                 data = json.load(f)
-                tasks = [Task(id=item["id"], payload=item["payload"]) for item in data]
+                tasks = [Task(id=item["id"], payload=item["payload"], description=item["payload"]["description"], priority=item["priority"]) for item in data]
             return tasks
         except Exception as e:
             raise ValueError(f"Error: {e}")
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,26 +9,33 @@ def test_run_tasks(tmp_path):
             "id": 1,
             "payload": {
             "order_id": 5501,
-            "amount": 1200
-            }
+            "amount": 1200,
+            "description": "Task description"
+            },
+            "priority": random.randint(1, 5)
         },
         {
             "id": 2,
             "payload": {
             "order_id": 5502,
-            "amount": 450
-            }
+            "amount": 450,
+            "description": "Task description"
+            },
+            "priority": random.randint(1, 5)
         },
         {
             "id": 3,
             "payload": {
             "order_id": 5503,
-            "amount": 3100
-            }
+            "amount": 3100,
+            "description": "Task description"
+            },
+            "priority": random.randint(1, 5)
         }
     ]
 
-    dir = tmp_path/"data".mkdir()
+    dir = tmp_path/"data"
+    dir.mkdir()
     file = dir/"data.json"
     
     with open(file, "w", encoding="utf-8") as f:
@@ -57,3 +64,39 @@ def test_file_task_source_not_found():
         source.get_tasks()
     
     assert "Error" in str(err.value)
+
+def test_task_creation_and_properties():
+    """Проверка создания задачи и работы свойств"""
+    task = Task(id=1, description="Task description", priority=4, payload={"order_id": 232})
+    
+    assert task.id == 1
+    assert task.description == "Task description"
+    assert task.priority == 4
+    assert task.status == 'created'
+    assert isinstance(task.created_at, datetime.datetime)
+    assert task.order_id == 232
+
+def test_task_invalid_id():
+    """Проверка валидации дескриптора IntegerRange для id"""
+    with pytest.raises(ValueError):
+        Task(id=0, description="Task description", priority=3)
+    with pytest.raises(TypeError):
+        Task(id="1", description="Task description", priority=3)
+
+def test_task_invalid_priority():
+    """Проверка валидации дескриптора IntegerRange для priority (min/max)"""
+    with pytest.raises(ValueError):
+        Task(id=1, description="Valid Desc", priority=6)
+
+def test_task_invalid_description():
+    """Проверка валидации свойства description"""
+    with pytest.raises(ValueError):
+        Task(id=1, description="A", priority=3)
+
+def test_task_ready_to_start():
+    """Проверка вычисляемого свойства ready_to_start"""
+    task = Task(id=1, description="Task description", priority=3)
+    assert task.ready_to_start is True
+    
+    task.status = 'in_progress'
+    assert task.ready_to_start is False
